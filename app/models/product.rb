3ed8bfac68d3efccab5c3fb0433908966aea696b
covalt-ta <taku.covayashi@gmail.com>
2020-08-12 14:35:55 +0900

    Admin, Productモデルのアソシエーション、バリデーション設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,13 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to :admin
 
+  with_options presence: true do
+    validates :name, length: { maxmum: 40 }
+    validates :text, length: { maxmum: 1000 }
+    validates :price
+    validates :category_id, numericality: { other_than: 1}
+    validates :admin_id
+  end
 end
