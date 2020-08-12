e537efd0c7b66665007386387606235a5a8909de
covalt-ta <taku.covayashi@gmail.com>
2020-08-12 15:31:41 +0900

    gem annotateインストール

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,32 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string(255)      not null
+#  price       :integer          not null
+#  text        :text(65535)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  admin_id    :bigint           not null
+#  cotegory_id :integer          not null
+#
+# Indexes
+#
+#  index_products_on_admin_id  (admin_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (admin_id => admins.id)
+#
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to :admin
 
   with_options presence: true do
-    validates :name, length: { maxmum: 40 }
-    validates :text, length: { maxmum: 1000 }
+    validates :name, length: { maximum: 40 }
+    validates :text, length: { maximum: 1000 }
     validates :price
     validates :category_id, numericality: { other_than: 1}
     validates :admin_id
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string(255)      not null
+#  price       :integer          not null
+#  text        :text(65535)      not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  admin_id    :bigint           not null
+#  cotegory_id :integer          not null
+#
+# Indexes
+#
+#  index_products_on_admin_id  (admin_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (admin_id => admins.id)
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
