29c628896b101b2b2687244b7c07fe252d645f7c
covalt-ta <taku.covayashi@gmail.com>
2020-08-13 19:30:54 +0900

    Payjpのインストール

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products, only: %i(show)
+  resources :card, only: [:new, :create]
   namespace :admins do
     root to: "dashboard#index"
     resources :products, only: %i(new create)
