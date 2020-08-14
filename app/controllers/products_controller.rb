70f721a4aff2d8b041cc6cdf6402abbe1c363cb2
covalt-ta <taku.covayashi@gmail.com>
2020-08-14 11:10:28 +0900

    商品詳細ページの作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,7 @@ class ProductsController < ApplicationController
     @products = Product.includes(:admin).order(created_at: :DESC)
   end
 
+  def show
+    @product = Product.find(params[:id])
+  end
 end
