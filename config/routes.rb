fe350d5f4724aac2a667dc8066601a7af2504ff0
covalt-ta <taku.covayashi@gmail.com>
2020-08-14 15:22:46 +0900

    買い物かご機能実装の為のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'cards/new'
   devise_for :admins
   devise_for :users
   root 'products#index'
-  resources :products, only: %i(show)
+
+  resources :products, only: %i(show) do
+    scope module: :products do
+      resources :add_to_baskets, only: :create
+      resources :delete_in_baskets, only: :create
+    end
+  end
+
   resources :users, only: %i(show update)
   resources :cards, only: %i(new create)
 
