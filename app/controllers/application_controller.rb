9797a0a71dc7fb802499fa0643044088ff158064
covalt-ta <taku.covayashi@gmail.com>
2020-08-16 20:32:06 +0900

    basic認証を導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+  before_action :basic_auth
+
   def after_sign_in_path_for(resource)
     if resource.is_a?(User)
       root_path
@@ -9,8 +10,14 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  protected
+  private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
   end
 end
