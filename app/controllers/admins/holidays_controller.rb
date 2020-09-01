d3aff8685cb9856afff5e804ffa1da6b69a4191f
covalt-ta <taku.covayashi@gmail.com>
2020-09-01 12:37:18 +0900

    shop_idの取得方法の編集

--- app/controllers/admins/holidays_controller.rb
+++ app/controllers/admins/holidays_controller.rb
@@ -1,7 +1,7 @@
 class Admins::HolidaysController < Admins::ApplicationController
 
   def create
-    if Holiday.create(holiday_params)
+    if Holiday.create!(holiday_params)
       redirect_to admins_shop_path(current_admin.shop), notice: "休日を登録しました"
     else
       redirect_to admins_shop_path(current_admin.shop), alert: "休日登録に失敗しました"
@@ -19,6 +19,6 @@ class Admins::HolidaysController < Admins::ApplicationController
 
   private
   def holiday_params
-    params.require(:holiday).permit(:day).merge(shop_id: 1)
+    params.require(:holiday).permit(:day).merge(shop_id: current_admin.shop.id)
   end
 end
