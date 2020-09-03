0eee61c083a0c92baa4f1e2e1428611abcb215ff
covalt-ta <taku.covayashi@gmail.com>
2020-09-03 14:47:20 +0900

    不要なコントローラーの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
-  }
+  devise_for :admins, controllers: {sessions: 'admins/sessions'}
   devise_scope :admin do
     get "sign_in", :to => "admins/sessions#new"
   end
