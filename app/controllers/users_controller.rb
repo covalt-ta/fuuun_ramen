823316acf24fc89bf4142953b96c4e7598ebdafd
covalt-ta <taku.covayashi@gmail.com>
2020-09-15 13:09:39 +0900

    テストユーザーの編集制限、他ユーザーのページ閲覧制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user
+  before_action :check_test_user, only: :update
+
   def show
     Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     if Card.find_by(user_id: current_user.id).present?
@@ -11,7 +14,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if current_user.update!(user_params)
+    if current_user.update(user_params)
       redirect_to user_path(current_user), notice: "ユーザー情報を更新しました"
     else
       redirect_to user_path(current_user), alert: "ユーザー情報を更新できませんでした"
@@ -21,5 +24,18 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:nickname, :email, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday)
+  end
+
+  def ensure_correct_user
+    if current_user.id != params[:id].to_i
+      flash[:alert] = "閲覧権限がありません"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def check_test_user
+    if current_user.email == "test@test.jp"
+      redirect_to user_path(current_user), alert: "テストユーザーの為、編集できません"
+    end
   end
 end
