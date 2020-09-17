86da1e65e462b833a275fa767623538ea0f83b9a
covalt-ta <taku.covayashi@gmail.com>
2020-09-17 18:14:56 +0900

    product_eatsコントローラーの単体テスト

--- spec/requests/product_eats_request_spec.rb
+++ spec/requests/product_eats_request_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "ProductEats", type: :request do
+  let(:user) { create(:user) }
+  let!(:product) { create(:product) }
+  let!(:product_eat) { create(:product_eat)}
+  before { sign_in user }
 
+  describe "食べた！を作成するリクエスト (POST #create)" do
+    it '正常にリクエストが成功すること' do
+      post product_product_eats_path(product), xhr: true
+      expect(response.status).to eq 200
+    end
+    it '食べた！が1つ増える' do
+      expect do
+        post product_product_eats_path(product), xhr: true
+      end.to change { ProductEat.count }.by(1)
+    end
+  end
+
+  describe '食べた！を取り消すリクエスト（DELETE #destroy）' do
+    before { post product_product_eats_path(product), xhr: true }
+    it '正常にリクエストが成功すること' do
+      delete product_product_eat_path(product, product_eat), xhr: true
+      expect(response.status).to eq 200
+    end
+    it '食べた！が1つ減る' do
+      expect do
+        delete product_product_eat_path(product, product_eat), xhr: true
+      end.to change { ProductEat.count }.by(-1)
+    end
+  end
 end
