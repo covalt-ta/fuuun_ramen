749a99af689610e97dd9e59957735eaf16cd46b1
covalt-ta <taku.covayashi@gmail.com>
2020-09-23 01:23:49 +0900

    おすすめ商品の点数変更

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -13,7 +13,7 @@ class BasketsController < ApplicationController
   def set_recommend_product
     basket_product_ids = current_user.prepare_basket.product_toppings.map {|p|  p.product_id}
     recommend_product_ids = Product.set_ranking - basket_product_ids
-    @recommend_product = Product.where(id: recommend_product_ids)
+    @recommend_product = Product.where(id: recommend_product_ids).first(9)
     if @recommend_product.count <= 3
       @recommend_product = Product.where(display: true).order(created_at: :DESC).first(6)
     end
