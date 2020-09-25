aaefb4bf68c90324615e29d87fe9132878fe9371
covalt-ta <taku.covayashi@gmail.com>
2020-09-25 23:54:40 +0900

    客数を取得の為、value追加

--- app/models/count_person.rb
+++ app/models/count_person.rb
@@ -1,15 +1,15 @@
 class CountPerson < ActiveHash::Base
   self.data = [
-    { id: 1, name: 'Take Out'},
-    { id: 2, name: '1名'},
-    { id: 3, name: '2名'},
-    { id: 4, name: '3名'},
-    { id: 5, name: '4名'},
-    { id: 6, name: '5名'},
-    { id: 7, name: '6名'},
-    { id: 8, name: '7名'},
-    { id: 9, name: '8名'},
-    { id: 10, name: '9名'},
-    { id: 11, name: '10名'}
+    { id: 1, name: 'Take Out', value: 0},
+    { id: 2, name: '1名', value: 1},
+    { id: 3, name: '2名', value: 2},
+    { id: 4, name: '3名', value: 3},
+    { id: 5, name: '4名', value: 4},
+    { id: 6, name: '5名', value: 5},
+    { id: 7, name: '6名', value: 6},
+    { id: 8, name: '7名', value: 7},
+    { id: 9, name: '8名', value: 8},
+    { id: 10, name: '9名', value: 9},
+    { id: 11, name: '10名', value: 10}
   ]
 end
