1f88a8308421215105b1c20d960fec37a4823329
covalt-ta <taku.covayashi@gmail.com>
2020-09-27 19:00:42 +0900

    管理者機能のビューファイル調整

---
+++ app/controllers/admins/sales_records_controller.rb
@@ -0,0 +1,11 @@
+class Admins::SalesRecordsController < Admins::ApplicationController
+  def index
+
+  end
+
+  private
+  def get_annual_date
+    # @annual_date = []
+    # month = { sales: current_admin.sales_this_month, customers: }
+  end
+end
