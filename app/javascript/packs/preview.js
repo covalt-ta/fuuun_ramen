1f265a0f5cc74f06247715c010d7e50057b907a4
covalt-ta <taku.covayashi@gmail.com>
2020-10-20 16:42:08 +0900

    preview.jsの修正

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,20 +1,25 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const ImageList = document.getElementById('product-image-list');
-  document.getElementById('product_image').addEventListener('change', function (e) {
-    // 画像が表示されている場合のみ、すでに存在している画像を削除する
-    const imageContent = document.querySelector('img');
-    if (imageContent) {
-      imageContent.remove();
-    }
-    const file = e.target.files[0]
-    const blob = window.URL.createObjectURL(file);
+if (document.URL.match(/products\/new/)) {
+  document.addEventListener('DOMContentLoaded', function () {
+    const ImageList = document.getElementById('product-image-list');
+    document.getElementById('product_image').addEventListener('change', function (e) {
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      const divContent = document.querySelector('.product-image-wrapper')
+      if (imageContent && divContent) {
+        imageContent.remove();
+        divContent.remove();
+      }
+      const file = e.target.files[0]
+      const blob = window.URL.createObjectURL(file);
 
-    const imageElement = document.createElement('div');
-    const blobImage = document.createElement('img');
-    blobImage.setAttribute('src', blob);
+      const imageElement = document.createElement('div');
+      const blobImage = document.createElement('img');
+      imageElement.setAttribute('class', 'product-image-wrapper')
+      blobImage.setAttribute('src', blob);
+      blobImage.setAttribute('class', 'product-image');
 
-    imageElement.appendChild(blobImage);
-    ImageList.appendChild(imageElement);
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
   });
-});
-
+}
