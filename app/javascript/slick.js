9131cbfa7306dff2e43f8470be9b42a65bc79dad
covalt-ta <taku.covayashi@gmail.com>
2020-11-01 22:03:34 +0900

    買物かごのレスポンシブ対応

--- app/javascript/slick.js
+++ app/javascript/slick.js
@@ -1,10 +1,24 @@
 $(function () {
   $('.slider').slick({
-    prevArrow: '<i class="fas fa-chevron-circle-left"></i>',
-    nextArrow: '<i class="fas fa-chevron-circle-right"></i>',
+    prevArrow: '<span class="prev-arrow"><i class="fas fa-chevron-circle-left"></i></span>',
+    nextArrow: '<span class="next-arrow"><i class="fas fa-chevron-circle-right"></i></span>',
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1010,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 510,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   });
 });
