d24ef2e60e143304928c2493759e3d037160c477
covalt-ta <taku.covayashi@gmail.com>
2020-11-07 22:42:11 +0900

    マイグレーションファイルの編集

--- db/migrate/20201107131113_create_rooms.rb
+++ db/migrate/20201107131113_create_rooms.rb
@@ -1,7 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-
+      t.references :reservation, null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20201107131258_create_messages.rb
+++ db/migrate/20201107131258_create_messages.rb
@@ -1,7 +1,10 @@
 class CreateMessages < ActiveRecord::Migration[6.0]
   def change
     create_table :messages do |t|
-
+      t.references :room, null: false, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :admin, foreign_key: true
+      t.text :message, null: false
       t.timestamps
     end
   end
