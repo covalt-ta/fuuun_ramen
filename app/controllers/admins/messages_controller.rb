9876e745f762c1705a893bb7f979452b2b1afa75
covalt-ta <taku.covayashi@gmail.com>
2020-11-09 21:51:42 +0900

    admins_messagesコントローラーの作成

--- app/controllers/admins/messages_controller.rb
+++ app/controllers/admins/messages_controller.rb
@@ -1,4 +1,15 @@
 class Admins::MessagesController < Admins::ApplicationController
   def create
+    message = Message.new(message_params)
+    if message.save
+      redirect_to admins_room_path(message.room.reservation_id)
+    else
+      redirect_to admins_room_path(message.room.reservation_id), alert: 'メッセージを送信できませんでした'
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:message, :room_id).merge(admin_id: current_admin.id)
   end
 end
